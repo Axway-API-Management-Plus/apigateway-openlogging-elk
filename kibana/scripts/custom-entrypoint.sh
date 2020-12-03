6961a24a28a2580b10c790828536532aeaeacadd
Chris Wiechmann <cwiechmann@axway.com>
2020-12-03 21:39:04 +0100

    Fixed script to support multiple ES-Hosts for Kibana
    Fixes #55

--- kibana/scripts/custom-entrypoint.sh
+++ kibana/scripts/custom-entrypoint.sh
@@ -3,11 +3,11 @@
 # This scripts adjusts the configured list of Elasticsearch hosts
 # Kibana requires each host to be double quoted 
 
-hostsForKibana=`echo ${ELASTICSEARCH_HOSTS} | awk '{split($0, va, /,/); vl=""; for (v in va) { if (vl =="" ) vl = sprintf("\"%s\"", va[v]); else vl = vl sprintf(",\"%s\"", va[v]); } print(vl) }'`
+hostsForKibana=`echo ${ELASTICSEARCH_HOSTS} | awk '{split($0, va, /,/); vl=""; for (v in va) { if (vl =="" ) vl = sprintf("\"%s\"", va[v]); else vl = vl sprintf(",\"%s\"", va[v]); } printf("'[%s]'", vl) }'`
 
 echo "Adjusted given Elasticsearch hosts: ${hostsForKibana} for Kibana"
 
-export ELASTICSEARCH_HOSTS=[$hostsForKibana]
+export ELASTICSEARCH_HOSTS=$hostsForKibana
 
 # Finally call the original Docker-Entrypoint
 /usr/local/bin/kibana-docker
