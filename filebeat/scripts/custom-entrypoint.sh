ed5f0a590f56b800495a3b62f956883c3c8af7c3
Chris Wiechmann <cwiechmann@axway.com>
2021-01-20 13:33:24 +0100

    [skip ci] Deprecation warning and error handling

--- filebeat/scripts/custom-entrypoint.sh
+++ filebeat/scripts/custom-entrypoint.sh
@@ -3,6 +3,16 @@
 # This script adjusts the configured list of Logstash hosts
 # as is it required by Filebeat. 
 
+if [ -z "${LOGSTASH_HOSTS}" ];then
+        if [ -z "${LOGSTASH}" ]; then
+		echo "Parameter: LOGSTASH_HOSTS is missing";
+		exit 99;
+	else 
+                echo "Parameter LOGSTASH is deprecated. Please migrate your configuration to parameter: LOGSTASH_HOSTS";
+                LOGSTASH_HOSTS=${LOGSTASH}
+        fi
+fi
+
 hostsForFilebeat=`echo ${LOGSTASH_HOSTS} | awk '{split($0, va, /,/); vl=""; for (v in va) { if (vl =="" ) vl = sprintf("\"%s\"", va[v]); else vl = vl sprintf(",\"%s\"", va[v]); } printf("'[%s]'", vl) }'`
 
 echo "Adjusted given Logstash hosts: ${hostsForFilebeat} for Filebeat"
