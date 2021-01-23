316e3f7fc156ee06b2c293935a205efa080fd5f9
Chris Wiechmann <cwiechmann@axway.com>
2021-01-23 21:21:04 +0100

    Initial version of Shell-Script tests

--- metricbeat/scripts/metricbeat-entrypoint.sh
+++ metricbeat/scripts/metricbeat-entrypoint.sh
@@ -2,6 +2,10 @@
 
 # This script adjusts the configured list of Elasticsearch hosts as required by Metricbeat
 
+
+# Save the originally given parameters as they are forwarded to the original docker-entrypoint script
+params=$@
+
 if [ "${METRICBEAT_ENABLED}" = "false" ];then
     echo "Metricbeat is disabled as parameter METRICBEAT_ENABLED is set to false";
     exit 88;
@@ -10,7 +14,7 @@ fi
 if [ -z "${METRICBEAT_USERNAME}"  ] || [ -z "${METRICBEAT_PASSWORD}"  ];then
     if [ "${ELASTICSEARCH_ANONYMOUS_ENABLED}" = "false" ];then
         echo "ELASTICSEARCH_ANONYMOUS_ENABLED is false, but parameter: METRICBEAT_USERNAME or METRICBEAT_PASSWORD is missing";
-        exit 99;
+        exit 98;
     fi
 fi
 
@@ -25,35 +29,59 @@ if [ -z "${KIBANA_HOST}" ];then
     export KIBANA_HOST=$kibanaHost
 fi
 
-elasticHosts=`echo ${ELASTICSEARCH_HOSTS} | awk '{split($0, va, /,/); vl=""; for (v in va) { if (vl =="" ) vl = sprintf("\"%s\"", va[v]); else vl = vl sprintf(",\"%s\"", va[v]); } printf("'[%s]'", vl) }'`
+elasticHosts=`echo ${ELASTICSEARCH_HOSTS} | awk '
+    function ltrim(s) { sub(/^[ \t\r\n]+/, "", s); return s }
+    function rtrim(s) { sub(/[ \t\r\n]+$/, "", s); return s }
+    function trim(s)  { return rtrim(ltrim(s)); }
+    {
+        split($0, va, /,/); vl=""; for (v in va) { 
+            if (vl =="" ) 
+                vl = sprintf("\"%s\"", trim(va[v])); 
+            else 
+                vl = vl sprintf(",\"%s\"", trim(va[v])); 
+        } 
+        printf("'[%s]'", vl) 
+    }'
+`
 
 echo "Adjusted given Elasticsearch hosts: ${elasticHosts} for Metricbeat"
 
-echo "KIBANA_HOST set to: ${KIBANA_HOST}";
+# Check if Kibana-Host is reachable and if, enable Kibana-Monitoring
+curl -skfv ${KIBANA_HOST} || rc=$?
 
-# Save the originally given parameters
-params=$@
+if [ "$rc" != "0" ];then
+    echo "KIBANA_HOST: ${KIBANA_HOST} is not reachable. Got returncode: ${rc} for command: curl -kv ${KIBANA_HOST}";
+    echo "Metricbeat Kibana monitoring will be disabled on this host.";
+    export METRICBEAT_ENABLE_KIBANA=false;
+else 
+    echo "Successfully connected to Kibana-Host: ${KIBANA_HOST}. Enable Metricbeat monitoring."
+    export METRICBEAT_ENABLE_KIBANA=true;
+    # Only if Kibana is reachable, try to load Dashboards is enabled
+    if [ "${METRICBEAT_SETUP_DASHBOARDS}" != "false" ];then
+        echo "Loading Metricbeat Dashboards into Kibana";
+        # Get the config-file
+        for i in "$@"
+        do
+            case $i in
+                -c)
+                    shift
+                    configFile="$1"
+                    break
+                    ;;
+                *)
+                    shift
+                    ;;
+            esac
+        done
+        metricbeat setup --strict.perms=false -e -c ${configFile}
+    fi
+fi
 
 export ELASTICSEARCH_HOSTS=$elasticHosts
 
-if [ "${METRICBEAT_SETUP_DASHBOARDS}" != "false" ];then
-    echo "Loading Metricbeat Dashboards into Kibana";
-    # Get the config-file
-    for i in "$@"
-    do
-        case $i in
-            -c)
-                shift
-                configFile="$1"
-                break
-                ;;
-            *)
-                shift
-                ;;
-        esac
-    done
-    metricbeat setup --strict.perms=false -e -c ${configFile}
+# Stop here, if script is tested with bats
+if [ -z "${BATS_TEST_FILENAME}" ]; then
+    exit 0
 fi
-
 # Finally call the original Docker-Entrypoint
 /usr/local/bin/docker-entrypoint $params
