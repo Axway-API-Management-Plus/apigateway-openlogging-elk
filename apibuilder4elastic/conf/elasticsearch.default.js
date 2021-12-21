5ddb08cd903720e111359cbbe698de2ad86cc3a3
Chris Wiechmann <cwiechmann@axway.com>
2021-12-21 11:30:13 +0100

    APIBuilder4Elastic, only read ES-CA if given
    #156

--- apibuilder4elastic/conf/elasticsearch.default.js
+++ apibuilder4elastic/conf/elasticsearch.default.js
@@ -19,7 +19,7 @@ module.exports = {
 				maxRetries: 5,
 				requestTimeout: 60000,
 				ssl: {
-					ca: fs.readFileSync(process.env.ELASTICSEARCH_CA),
+					ca: ("false" == process.env.ELASTICSEARCH_SSL_VERIFICATIONMODE) ? fs.readFileSync(process.env.ELASTICSEARCH_CA) : "",
 					rejectUnauthorized: ("false" == process.env.ELASTICSEARCH_SSL_VERIFICATIONMODE) ? false : true
 				}
 			},
